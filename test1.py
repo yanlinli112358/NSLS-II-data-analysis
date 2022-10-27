06dce899b7b3a0e7e349d83b39c08180f9b442d9
yanlinli112358 <116615999+yanlinli112358@users.noreply.github.com>
2022-10-27 12:05:56 -0500

    add test file

---
+++ test1.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 14:37:47 2022
+
+@author: Rachel
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from scipy.integrate import quad
+from scipy.stats.mstats import chisquare
+
+#def gaussian function for fitting
+def gaussian(x, A, sig, miu):
+
+    y = A * np.exp(-1 * (x-miu)**2 / (2 * sig**2))
+    return y
+
+#def polynomial background
+def bkg(x, *coeffs):
+    n = 0
+    y = 0
+    while n < len(coeffs):
+        y += coeffs[n] * np.power(x, n)
+        n += 1
+    return y
+
+
+def signal_fit(x, num_peaks, bkg_order, *paras):
+    s = "def signal_total(x"
+                  
+    var_n = 0
+    for v in paras:
+        s += ", " 
+        s += "v"+str(var_n)
+        var_n += 1
+    s += '): \n'
+    s += "    y = bkg(x"
+    bkg_n = 0
+    while bkg_n <= bkg_order: 
+        s += ','
+        s += 'v' + str(bkg_n)
+        bkg_n +=1
+    s += ') \n' 
+
+    n = 0
+    while n < num_peaks:
+        s += "    y += gaussian(x "        
+        s += ','
+        s += 'v' + str(bkg_order + 1 + n*3) + ', v' + str(bkg_order + 2 + n*3) + ', v' + str(bkg_order + 3 + n*3)
+        s += ')\n'
+        n += 1
+    
+    s += \
+    "    return y"
+    print(s)
+    exec(s, globals())
+    
+    s2 = "y = signal_total(x"
+    for p in paras:
+        s2+= ','
+        s2 += str(p)
+    s2 += ') \n'
+    print(s2)
+    exec(s2, globals())
+    
+    print(y[2])
+
+x = np.array([1, 2, 43, 6])
+signal_fit(x, 1, 1, 2.0, 4.0, 3.0, 4., 5.)

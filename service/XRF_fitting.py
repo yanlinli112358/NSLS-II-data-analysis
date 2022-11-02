1943ae3378a2bba4864a136dc5b93d73a767c6d2
yanlinli112358 <yanlinli2022@u.northwestern.edu>
2022-11-02 17:27:43 -0500

    separate fit from service. fixed exec global(), first working version

--- service/XRF_fitting.py
+++ service/XRF_fitting.py
@@ -14,13 +14,14 @@ sys.path.append('../')
 from utils.input_output import get_data
 from utils.input_output import get_all_data
 from utils.input_output import get_qz
+from utils.fit_functions import signal_fit
 
 #specify path
 os.chdir('/Users/rachel/NSLS_II_beamtrips/2022_10_trip_shared')
 
 #inputs
 ##file reading and writing directory
-file = 's2_2-b3aa2f9a-1199-4a3d-b9af-e05292c50c70'
+file = 's2_10mMKBr_2-b3aa2f9a-1199-4a3d-b9af-e05292c50c70'
 #filename = 'fluo_data/' + file + '.txt'
 filename = 'fluo_data_all/' + file + '.txt'
 Qz_name = 'fluo_data_all/Qz-' + file + '.txt'
@@ -32,124 +33,16 @@ high_e = 12200
 num_peaks = 1
 bkg_order = 1
 peak_centers = [11920]
-
+scale_factor = 2500000.
 
 # produce the total photon counts in the full spectra
 total_I = get_all_data(filename, 10, 40950)
 total_counts = sum(sum(filter(lambda x: x>3, i)) for i in total_I)
 print('totanumber of photon recieved by the detector is ' + str(int(total_counts)))
 
-
-#def gaussian function for fitting
-def gaussian(x, A, sig, miu):
-    y = A * np.exp(-1 * (x-miu)**2 / (2 * sig**2))
-    return y
-
-#def polynomial background
-def bkg(x, *coeffs):
-    n = 0
-    y = 0
-    while n < len(coeffs):
-        y += coeffs[n] * x**n
-        n += 1
-    return y
-
-
-#def error processing
-def r_square(x, fit_x):
-    mean = np.sum(x)/len(x)
-    ss_res = np.sum((x-fit_x)**2)
-    ss_tot = np.sum((x - mean)**2)
-    r_square = 1 - ss_res/ss_tot
-    return r_square
-
-
-
-#def sum of peaks and curves   
-def signal_fit(x, y, num_peaks, bkg_order, peak_centers):
-    #transform the function to a function with fixed number of parameters
-    s = "def signal_total(x"
-                  
-    var_n = 0
-    while var_n <= (num_peaks * 3 + bkg_order):
-        s += ", " 
-        s += "v"+str(var_n)
-        var_n += 1
-    s += '): \n'
-    s += "    y = bkg(x"
-    bkg_n = 0
-    while bkg_n <= bkg_order: 
-        s += ','
-        s += 'v' + str(bkg_n)
-        bkg_n +=1
-    s += ') \n' 
-
-    n = 0
-    while n < num_peaks:
-        s += "    y += gaussian(x "        
-        s += ','
-        s += 'v' + str(bkg_order + 1 + n*3) + ', v' + str(bkg_order + 2 + n*3) + ', v' + str(bkg_order + 3 + n*3)
-        s += ')\n'
-        n += 1
-    
-    s += \
-    "    return y"
-    #print(s)
-    exec(s, globals())
-    
-    #curve fit the transformed function 'signal_total'
-    ##determine initial guess and bounds
-    maxy = max(y)
-    p0_list = []
-
-    countb = 0
-    while countb <= bkg_order:
-        p0_list.append(0)
-        countb += 1
-    countg = 0
-    while countg < num_peaks:
-        p0_list.append(maxy)
-        p0_list.append(100)
-        p0_list.append(peak_centers[countg])
-        countg += 1
-
-
-    ##fit
-    para, pcov = curve_fit(signal_total, x, y, p0 = p0_list)
-    bkg_return = para[0: bkg_order + 1]
-    gaussian_return = para[bkg_order + 1:]
-    
-    # print('background parameters = ' + str(bkg_return))
-    # print('gaussian parameters = ' + str(gaussian_return))
-    
-    #calculate integrated intensity and error
-    A1 = gaussian_return[0]
-    sig1 = gaussian_return[1]
-    intg_I = A1 * sig1
-
-    A1_err = pcov[bkg_order + 1, bkg_order + 1]
-    sig1_err = pcov[bkg_order + 2, bkg_order + 2]
-    A1_sig1 = pcov[bkg_order + 1, bkg_order + 2]
-    err = np.sqrt(abs(A1_err) / A1 ** 2 + abs(sig1_err) / sig1 ** 2 + 2 / sig1 / A1 * np.sqrt(abs(A1_sig1))) * intg_I
-
-    #plot data
-    plt.scatter(x,y)
-    #plot fit
-    s2 = "fit_y = signal_total(x" 
-    for p in para:
-        s2 += ','
-        s2 += str(p)
-    s2+= ')\n'
-    exec(s2, globals())
-    
-    plt.plot(x, fit_y)
-    #return
-    return (intg_I, err)
-
-
 #plot the spectra
 x = np.linspace(low_e, high_e, (high_e - low_e)//10 + 1)
-I = get_all_data(filename, low_e, high_e)
+I = np.array(get_all_data(filename, low_e, high_e))/scale_factor
 # if len(I) == 17:
 #     Qz = get_qz('fluo_data/Qz_17.txt')
 # else:
@@ -159,7 +52,7 @@ plt.figure()
 plt.ylabel('Intensity (counts)')
 plt.xlabel('Energy (eV)')
 for y in I:
-    plt.scatter(x, y, linewidths= 0.2)
+    plt.scatter(x, y/scale_factor, linewidths= 0.2)
 plt.show()
 
 
@@ -182,14 +75,16 @@ integrated_I = []
 err_I = []
 width = []
 
-#plt.figure()
-plt.title(file + '_fit')
+plt.figure()
+plt.title('data vs fit')
 for y in I:
     y = np.array(y)
+    x = np.array(x)
     integrated_I_value, err_I_value = signal_fit(x, y, num_peaks, bkg_order, peak_centers)
     integrated_I.append(integrated_I_value)
     err_I.append(err_I_value)
-#plt.show()
+plt.show()
+
 
 print(len(integrated_I))
 print(len(err_I))
